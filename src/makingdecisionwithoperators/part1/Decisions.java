29853146f12790547f420e8b199a9317557e7caa
mishobo <60912349+mishobo@users.noreply.github.com>
2023-02-19 12:18:33 +0300

    first commit

---
+++ src/makingdecisionwithoperators/part1/Decisions.java
@@ -0,0 +1,16 @@
+package makingdecisionwithoperators.part1;
+
+public class Decisions {
+
+	public static void main(String[] args) {
+		int x = 6;
+		
+		if (x == 5) {
+			System.out.println("The value of x is 5");
+		} else {
+			System.out.println("The value of x is " + x);
+		}
+
+	}
+
+}
