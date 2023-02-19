e9a14da5e6337ef91f693ac8762d1ee82b4f6a25
mishobo <60912349+mishobo@users.noreply.github.com>
2023-02-19 13:14:41 +0300

    conditional statements

--- src/makingdecisionwithoperators/part1/Decisions.java
+++ src/makingdecisionwithoperators/part1/Decisions.java
@@ -4,11 +4,39 @@ public class Decisions {
 
 	public static void main(String[] args) {
 		int x = 6;
+		int y = 5;
+		int z = 7;
+		boolean a = true;
+		boolean b = false;
 		
+		
+		// comparison operator
 		if (x == 5) {
 			System.out.println("The value of x is 5");
+		}else if(y < z){
+			System.out.println(true);
 		} else {
 			System.out.println("The value of x is " + x);
+		}
+		
+		// and operator
+		if(a && b) {
+			System.out.println("condition is true");
+		} else {
+			System.out.println("condition is false");
+		}
+		
+		if ((x<z) && (a)) {
+			System.out.println("And condition is true");
+		}
+		
+		// not operator
+		if(!b) {
+			System.out.println("not true: converts a false statement to true");
+		}
+		
+		if(!a || !b) {
+			System.out.println("a or b is true");
 		}
 
 	}
